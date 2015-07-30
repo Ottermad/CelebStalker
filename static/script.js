184bc128c0ece8c9362be07b74307fe2a979d67c
Mikhail Datoo <mdatoo@Arifs-MacBook-Pro.local>
2015-07-30 11:47:46 +0100

    For server - changes

--- static/script.js
+++ static/script.js
@@ -1,5 +1,4 @@
 $('img').on('dragstart', function(event) { event.preventDefault(); });
 
-navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude);
-});
+
+
--- web/static/script.js
+++ web/static/script.js
@@ -1,5 +1,1 @@
 $('img').on('dragstart', function(event) { event.preventDefault(); });
-
-navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude);
-});

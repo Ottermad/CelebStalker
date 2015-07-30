31a998a075cbede56b7fb2b5b726c82a01cb8aa1
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-07-30 14:46:37 +0100

    First two pages kind of work

--- web/__init__.py
+++ web/__init__.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+
+import requests
 
 app = Flask(__name__)
+
+base_url = 'http://6a8fd9ce.ngrok.com/'
 
 @app.route('/travel')
 def travel():
 	return render_template('travel.html')
 
 
-@app.route('/events')
-def events():
-	return render_template('events.html')
+@app.route('/events/<celeb>')
+def events(celeb):
+	r = requests.get(base_url + 'events/search/' + celeb)
+	json_data = r.json()
+	# id, date, maxprice currency, name
+	data = []
+	for event in json_data['events']:
+		e_data = {}
+		e_data['id'] = event['id']
+		e_data['date'] = event['localeventdate']
+		e_data['name'] = event['name']
+		e_data['price'] = event['price_ranges']['including_ticket_fees']['max']
+		data.append(e_data)
+	return render_template('events.html', events=data)
 
 
 @app.route('/hotels')
@@ -17,8 +32,11 @@ def hotels():
 	return render_template('hotels.html')
 
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def home():
+	if request.method == 'POST':
+		celeb = request.form['searchText']
+		return redirect(url_for('events', celeb=celeb))
 	return render_template('index.html')
 
 if __name__ == '__main__':
